c38fef3c792339ef0e5e699b835f3ca068b07e0c
Andreas Piening <andreaspiening@catalyst.net.nz>
2016-12-13 14:43:22 +1300

    WFS Polygons working

--- javascript/OL3MapPage-init.js
+++ javascript/OL3MapPage-init.js
@@ -1,5 +1,11 @@
 (function($) {
 
+    var projection1 = 'EPSG:3857';
+    var projection2 = 'EPSG:4326';
+    var projection = projection1;
+    var projectionO = projection1 == projection ? projection2 : projection1;
+
+
     var target = $('#map');
     var setup = {
         view: target.data('view'),
@@ -17,59 +23,62 @@
     layers.push(new ol.layer.Tile({
         source: new ol.source.TileWMS({
             url: 'http://wms.niwa.co.nz/cgi-bin/bnz_demo',
-            projection: 'EPSG:4326',
+            projection: projection2,
             params: { 'LAYERS': 'coast_poly' }
         })
     }));
 
-    // // background layer 2: bathymetry
-    // layers.push(new ol.layer.Tile({
-    //     source: new ol.source.TileWMS({
-    //         url: 'http://wms.niwa.co.nz/cgi-bin/cc_context',
-    //         projection: 'EPSG:4326',
-    //         params: { 'LAYERS': 'contours_z1' }
-    //     })
-    // }));
+    // background layer 2: bathymetry
+    layers.push(new ol.layer.Tile({
+        source: new ol.source.TileWMS({
+            url: 'http://wms.niwa.co.nz/cgi-bin/cc_context',
+            projection: 'EPSG:4326',
+            params: { 'LAYERS': 'contours_z1' }
+        })
+    }));
 
-    // // background layer 3: NZ EEZ
-    // layers.push(new ol.layer.Tile({
-    //     source: new ol.source.TileWMS({
-    //         url: 'http://wms.niwa.co.nz/cgi-bin/nz_poly',
-    //         projection: 'EPSG:4326',
-    //         params: { 'LAYERS': 'EEZ' }
-    //     })
-    // }));
+    // background layer 3: NZ EEZ
+    layers.push(new ol.layer.Tile({
+        source: new ol.source.TileWMS({
+            url: 'http://wms.niwa.co.nz/cgi-bin/nz_poly',
+            projection: 'EPSG:4326',
+            params: { 'LAYERS': 'EEZ' }
+        })
+    }));
 
     var layerWFS = new ol.layer.Vector({
         source: new ol.source.Vector({
-            loader: function(extent) {
-                $.ajax('http://demo.opengeo.org/geoserver/wfs', {
-                    type: 'GET',
-                    data: {
-                        service: 'WFS',
-                        version: '1.1.0',
-                        request: 'GetFeature',
-                        typename: 'water_areas',
-                        srsname: 'EPSG:3857',
-                        bbox: extent.join(',') + ',EPSG:3857'
-                    }
-                }).done(function(response) {
-                    layerWFS
-                    .getSource()
-                    .addFeatures(new ol.format.WFS()
-                    .readFeatures(response));
+            loader: function(extent, resolution, projection) {
+
+                var featureRequest = new ol.format.WFS().writeGetFeature({
+                    srsName: projection2,
+                    featureNS: 'http://www.opengis.net/gml',
+                    featureTypes: ['swath_poly'],
+                    outputFormat: 'text/xml; subtype=gml/3.1.1'
+                });
+                // then post the request and add the received features to a layer
+                fetch('/OL3Proxy/dorequest?u=http://wms.niwa.co.nz/cgi-bin/os2020_swath', {
+                    method: 'POST',
+                    body: new XMLSerializer().serializeToString(featureRequest)
+                }).then(function(response) {
+                    return response.text();
+                }).then(function(wfs) {
+                    var features = new ol.format.WFS().readFeatures(
+                        wfs, {
+                            dataProjection: projection2,
+                            featureProjection: projection1
+                        }
+                    );
+                    layerWFS.getSource().addFeatures(features);
                 });
             },
-            strategy:  ol.loadingstrategy.bbox,
-            projection: 'EPSG:3857'
-            // url: 'http://wms.niwa.co.nz/cgi-bin/os2020_swath'
-            // url: '/Proxy/dorequest?u=http://wms.niwa.co.nz/cgi-bin/os2020_swath'
+            projection: projection
         }),
 
         style: new ol.style.Style({
             stroke: new ol.style.Stroke({
-                color: 'rgba(0, 0, 255, 1.0)',
-                width: 2
+                color: 'rgba(100, 100, 150, 1.0)',
+                width: 1
             })
         })
     });
@@ -80,10 +89,9 @@ layers.push(layerWFS);
         target: target.get(0),
         layers: layers,
         view: new ol.View({
-            // center: ol.proj.fromLonLat([parseFloat(setup.view.Lon), parseFloat(setup.view.Lat)]),
-            // zoom: setup.view.Zoom
-            center: [-8908887.277395891, 5381918.072437216],
-            zoom: 14
+            projection: projection,
+            center: ol.proj.fromLonLat([parseFloat(setup.view.Lon), parseFloat(setup.view.Lat)]),
+            zoom: setup.view.Zoom
         })
     });
 
