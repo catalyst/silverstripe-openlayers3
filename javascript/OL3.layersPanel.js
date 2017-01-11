095ddaafcb6a3f4cd1bb97bceebd6916bcac8a30
Andreas Piening <andreaspiening@catalyst.net.nz>
2017-01-11 14:46:27 +1300

    ENHANCEMENT: separate popup rendering from displaying

--- javascript/OL3.layersPanel.js
+++ javascript/OL3.layersPanel.js
@@ -10,6 +10,8 @@ OL3.extend(function(){
             ol3.layersPanel.render(config ? config.element : undefined);
 
         },
+        iconSize: { width: 25, height: 25 },
+        iconPolygon: new ol.geom.Polygon([[[8, 3], [22, 1], [15, 12], [24, 24], [4, 24], [1, 13], [8, 3]]]),
         render: function(element) {
 
             var map = ol3.cache.map,
@@ -29,7 +31,19 @@ OL3.extend(function(){
 
                 var checkbox = new ol3.html('<input>').attr('type', 'checkbox').data('layer', layer),
                     title = '" ' + layer.get('Title') + '"',
-                    item = new ol3.html('<li>').append(new ol3.html('<label>').append(checkbox).append(title));
+                    item = new ol3.html('<li>'),
+                    itemContent = new ol3.html('<label>').append(checkbox).append(title),
+                    icon = ol3.layersPanel.getIconForLayer(layer);
+
+                if (icon) {
+                    item
+                        .append(new ol3.html(icon)
+                        .css({
+                            width: ol3.layersPanel.iconSize.width + 'px',
+                            height: ol3.layersPanel.iconSize.height + 'px'
+                        }));
+                }
+                item.append(itemContent);
 
                 if (layer.getVisible()) checkbox.attr('checked', 'checked');
 
@@ -45,6 +59,44 @@ OL3.extend(function(){
                 list.append(item);
 
             });
+        },
+        getIconForLayer: function(layer) {
+
+            if (!(layer instanceof ol.layer.Vector)) return;
+
+            var image,
+                style = layer.getStyle(),
+                type = layer.config.SourceType,
+                geos = {
+                    polygon: ol3.layersPanel.iconPolygon,
+                    line: new ol.geom.LineString([[0, ol3.layersPanel.iconSize.height], [ol3.layersPanel.iconSize.width, 0]]),
+                    point: new ol.geom.Point([ol3.layersPanel.iconSize.width / 2, ol3.layersPanel.iconSize.height / 2])
+                };
+
+            if (typeof style == 'function') style = style();
+
+            if (type == 'point') {
+                markerimage = style.getImage().getImage();
+                if (markerimage instanceof Image) {
+                    image = new Image();
+                    markerimage.addEventListener('load', function() { console.log(image, this.src); image.src = this.src; });
+                }
+            }
+            if (!(image instanceof Image)) {
+
+                var vectorContext,
+                    image = new ol3.html('<canvas>')
+                        .attr(ol3.layersPanel.iconSize)
+                        .css({ position: 'absolute', 'top': 0, 'left': 0, background: 'white' })
+                        .get();
+
+                vectorContext = ol.render.toContext(image.getContext('2d'), {size: [ol3.layersPanel.iconSize.width, ol3.layersPanel.iconSize.height]});
+
+                vectorContext.setStyle(style);
+                vectorContext.drawGeometry(geos[type]);
+            }
+
+            return image instanceof Image ? image : new ol3.html('<img>').prop('src', image.toDataURL('image/png'));
         }
     };
 });
